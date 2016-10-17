5fe41017681b6682740d21a6e694ad20b7925623
Rajeev Desai <rajeevdesai94@gmail.com>
2016-10-17 16:44:35 +0530

    New home screen. Replaced buttons with icons.

--- app/src/main/java/com/example/raviteja/addictionary/MainActivity.java
+++ app/src/main/java/com/example/raviteja/addictionary/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -17,6 +18,7 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean sound = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +30,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startMultiplayer(View view) {
-        Toast.makeText(this, "not yet functional", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Multiplayer not yet functional", Toast.LENGTH_SHORT).show();
     }
 
     public void displayUserWords(View view) {
-        Toast.makeText(this, "not yet functional", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "User Words not yet functional", Toast.LENGTH_SHORT).show();
+    }
+
+    public void settings(View view) {
+        Toast.makeText(this, "Settings not yet functional", Toast.LENGTH_SHORT).show();
+    }
+
+    public void sound(View view) {
+        ImageView img = (ImageView) findViewById(R.id.sound);
+        if(sound) {
+            sound = false;
+            img.setImageResource(R.drawable.ic_volume_off_black_24dp);
+        } else if(!sound) {
+            sound = true;
+            img.setImageResource(R.drawable.ic_volume_up_black_24dp);
+        }
+        //Toast.makeText(this, "Sounds not yet functional", Toast.LENGTH_SHORT).show();
     }
 
     private void navigateTo(Class c) {
